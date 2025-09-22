7238d320b8c23512052df260f2c410a5979f4d26
meenakshee23 <meenaksheegopaul@gmail.com>
2025-09-23 01:17:59 +0400

    new file

--- simple_shell.c
+++ simple_shell.c
@@ -9,92 +9,61 @@
 #include <errno.h>
 
 extern char **environ;
-
 /**
  * main - simple shell
- * Return: 0 on success
+ * Return: 0 success
  */
 int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t read;
+	ssize_t nread;
 	pid_t pid;
 	int status;
-	int interactive = isatty(STDIN_FILENO);
-	char *argv[64];
-	int argc;
-	int i;
-	char *token;
+	char *argv[2];
 
 	while (1)
 	{
-		if (interactive)
-		{
-			printf("simpleshell$ ");
-		}
+		printf("simpleshell$");
+		nread = getline(&line, &len, stdin);
 
-		read = getline(&line, &len, stdin);
-		if (read == -1)
+		if (nread == -1)
 		{
-			if (interactive)
-				printf("simpleshell$\n");
+			printf("simpleshell$\n");
 			break;
 		}
-
-		if (line[read - 1] == '\n')
-			line[read - 1] = '\0';
+		if (line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
 
 		if (line[0] == '\0')
 			continue;
 
-		argc = 0;
-		token = strtok(line, " ");
+		argv[0] = line;
+		argv[1] = NULL;
 
-		while (token != NULL && argc < 63)
-		{
-			argv[argc] = token;
-			token = strtok(NULL, " ");
-			argc++;
-		}
-
-		argv[argc] = NULL;
-		if (argc == 0)
-			continue;
-
-		if (strcmp(argv[0], "exit") == 0)
-			break;
-		if (strcmp(argv[0], "env") == 0)
-		{
-			for (i = 0; environ[i] != NULL; i++)
-				printf("%s\n", environ[i]);
-			continue;
-		}
-		if (argv[0][0] != '/' && !(argv[0][0] == '.' && argv[0][1] == '/'))
-		{
-			printf("Command not found\n");
-			continue;
-        }
 		pid = fork();
 		if (pid == -1)
 		{
 			perror("fork");
 			continue;
-        }
-
+		}
 		if (pid == 0)
 		{
 			if (execve(argv[0], argv, environ) == -1)
 			{
-				printf("Command not found\n");
+				perror("simpleshell");
 				exit(EXIT_FAILURE);
 			}
-        }
+		}
 		else
-        {
+		{
 			waitpid(pid, &status, 0);
 		}
-    }
+	}
+
 	free(line);
 	return (0);
 }
+
+
+
